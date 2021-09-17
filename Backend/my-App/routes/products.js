8600ecc0f0ab9e88bc16e60330c88cb2d2971448
Iberassa <indriasbelachew@gmail.com>
2021-09-17 18:53:19 -0400

    product change

--- Backend/my-App/routes/products.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const productsController = require('../controller/productsController');
-
-
-router.post('/', productsController.insertProduct);
-
-module.exports = router;
