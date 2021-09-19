1cc709dcc0b4703a4197562139cb033640a149a7
Amanuel Abuhay <aaamare36@gamil.com>
2021-09-19 10:03:59 -0500

    update customer

---
+++ Frontend/my-app/src/components/CustomersComponents/GetFarmers.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+class GetFarmers extends React.Component{
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default GetFarmers
